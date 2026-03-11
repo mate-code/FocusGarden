88a9b5c1a3d4ffe1ccd66da333c64de35541d50c
MPR235953 <mateuszxpruss@gmail.com>
2026-03-11 13:30:54 +0100

    refactor: changing names of XML components

--- app/src/main/java/com/matecode/focusgarden/FirstFragment.java
+++ app/src/main/java/com/matecode/focusgarden/FirstFragment.java
@@ -1,6 +1,7 @@
 package com.matecode.focusgarden;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +29,10 @@ public class FirstFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        binding.tvFocusTime.setGravity(Gravity.CENTER_VERTICAL | Gravity.CENTER_HORIZONTAL);    // recenter textView with declared focus time
+
+
 
         //binding.buttonFirst.setOnClickListener(v ->
         //        NavHostFragment.findNavController(FirstFragment.this)
